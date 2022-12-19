b9ff79cad9697df920938e9c99d12076e1476119
warshiah12 <warshiah1122@gmail.com>
2022-12-19 02:59:54 -0800

    BMI Calculator

--- app/src/main/java/com/example/bmi_calculator/alert.kt
+++ app/src/main/java/com/example/bmi_calculator/alert.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class alert : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_alert)
+        setContentView(R.layout.alert_box)
     }
 }
